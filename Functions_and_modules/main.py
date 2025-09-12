48b991be80c8d5dd62bff0b0f49bab3b07ba2413
ankitagrg <ankitalg18@gmail.com>
2025-09-12 21:57:51 +0545

    Add functions for module operations

---
+++ Functions_and_modules/main.py
@@ -0,0 +1,10 @@
+import my_module
+
+print(my_module.greet("Ankita"))
+print("Sum:", my_module.add(5, 10))
+
+# Example: Function defined here
+def mean(numbers):
+    return sum(numbers) / len(numbers)
+
+print("Mean:", mean([10, 20, 30]))
