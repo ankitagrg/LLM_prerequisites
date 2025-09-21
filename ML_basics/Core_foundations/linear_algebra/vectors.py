ddb5cddbdc2bec764267aa96d34a5f28efda3d1a
ankitagrg <ankitalg18@gmail.com>
2025-09-21 23:24:40 +0545

    Implement vector creation using NumPy arrays

---
+++ ML_basics/Core_foundations/linear_algebra/vectors.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Creating vectors
+v1 = np.array([1, 2, 3])
+v2 = np.array([4, 5, 6])
+
+print("Vector 1:", v1)
+print("Vector 2:", v2)
+
+# Vector addition
+print("Addition:", v1 + v2)
+
+# Scalar multiplication
+print("Scalar Multiplication:", 3 * v1)
