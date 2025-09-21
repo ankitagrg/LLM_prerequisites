3b3cea9b05879537743c1b20ffcd2fb982264b70
ankitagrg <ankitalg18@gmail.com>
2025-09-21 23:27:06 +0545

    Add NumPy examples for matrix operations: addition and transpose

---
+++ ML_basics/Core_foundations/linear_algebra/matrices.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Creating matrices
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+print("Matrix A:\n", A)
+print("Matrix B:\n", B)
+
+# Matrix addition
+print("A + B:\n", A + B)
+
+# Matrix transpose
+print("Transpose of A:\n", A.T)
