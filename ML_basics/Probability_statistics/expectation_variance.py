947acc6d266cfc71665bca7bb8eb78eb4e37e629
ankitagrg <ankitalg18@gmail.com>
2025-09-22 21:59:48 +0545

    Implement statistical example using NumPy

---
+++ ML_basics/Probability_statistics/expectation_variance.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+data = np.array([2, 4, 4, 4, 5, 5, 7, 9])
+
+mean = np.mean(data)
+variance = np.var(data)
+
+print("Data:", data)
+print("Mean:", mean)
+print("Variance:", variance)
